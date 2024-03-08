93dfb4e207b97c263b042a4584abfa969c2e5d89
kunat <kunat.kamprommapirak@gmail.com>
2024-03-08 19:17:49 +0700

    update email file

--- src/pages/api/emails.js
+++ src/pages/api/emails.js
@@ -3,23 +3,23 @@ import path from 'path';
 
 export default async function handler(req, res) {
   try {
-    const filePath = path.join(process.cwd(), 'public', 'data', 'emaillist.txt');
+    // const filePath = path.join(process.cwd(), 'public', 'data', 'emaillist.txt');
 
-    const data = await fs.readFile(filePath, 'utf-8');
+    // const data = await fs.readFile(filePath, 'utf-8');
     
-    var emailList = []
-    if(data){
-      const lines = data.split('\n');
-      lines.forEach((line, index) => {
-        console.log(`Line ${index + 1}: ${line}`);
-        if(line){
-          emailList.push(line)
-        }
-      });
-    }
+    // var emailList = []
+    // if(data){
+    //   const lines = data.split('\n');
+    //   lines.forEach((line, index) => {
+    //     console.log(`Line ${index + 1}: ${line}`);
+    //     if(line){
+    //       emailList.push(line)
+    //     }
+    //   });
+    // }
     
 
-    res.status(200).json({ success: true, data:emailList });
+    res.status(200).json({ success: true, data:"" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: 'เกิดข้อผิดพลาดในการอ่านไฟล์' });
